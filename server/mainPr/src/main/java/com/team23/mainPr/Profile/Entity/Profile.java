ceaa8a13c61717aea829cf454f825dcd5ac3ca24
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-09-13 22:10:50 +0900

    [etc] 엔티티의 id 값 생성 전략에 대해 나중에 바꾸도록 주석 추가

--- server/mainPr/src/main/java/com/team23/mainPr/Profile/Entity/Profile.java
+++ server/mainPr/src/main/java/com/team23/mainPr/Profile/Entity/Profile.java
@@ -6,6 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/*
+* refactor : @GeneratedValue(strategy = GenerationType.IDENTITY)는 적절하지 않다. - 불필요한 DB 엑세스 발생, 이후 해시값 사용 등 정말 유니크한 값을 만드는 방식을 취해야 한다.
+ * */
+
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
