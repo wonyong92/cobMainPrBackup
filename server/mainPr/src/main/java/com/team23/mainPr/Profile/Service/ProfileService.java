f503381c2bac82e6fa4beca119e677934a08e55a
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-09-13 09:40:40 +0900

    [feat] 기본 요청들에 대해 cors 설정 모두 풀어놓기

---
+++ server/mainPr/src/main/java/com/team23/mainPr/Profile/Service/ProfileService.java
@@ -0,0 +1,9 @@
+package com.team23.mainPr.Profile.Service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileService {
+
+}
