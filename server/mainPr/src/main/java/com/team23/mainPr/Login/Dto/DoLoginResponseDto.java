a42b3e3fc50bdbbe16e46b27140519f8f1106041
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-09-18 19:12:08 +0900

    프로젝트 초기화

--- server/mainPr/src/main/java/com/team23/mainPr/Login/Dto/DoLoginResponseDto.java
+++
@@ -1,4 +0,0 @@
-package com.team23.mainPr.Login.Dto;
-
-public class DoLoginResponseDto {
-}
--- server/mainPr/src/main/java/com/team23/mainPr/Profile/Repository/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team23.mainPr.Profile.Repository;
-
-import com.team23.mainPr.Profile.Entity.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Integer> {
-}
