ce8d0f53e7c578837ec71b121466793627e31527
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-09-18 19:54:00 +0900

    회원 도메인 구현

---
+++ server/mainPr/src/main/java/com/team23/mainPr/Domain/Member/Dto/FindIdDto.java
@@ -0,0 +1,9 @@
+package com.team23.mainPr.Domain.Member.Dto;
+
+import lombok.Data;
+
+@Data
+public class FindIdDto {
+    String email;
+    String name;
+}
---
+++ server/mainPr/src/main/java/com/team23/mainPr/Domain/Member/Dto/UpdateMemberDto.java
@@ -0,0 +1,11 @@
+package com.team23.mainPr.Domain.Member.Dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateMemberDto {
+
+    private String nickname;
+    private String profileImageId;
+
+}
