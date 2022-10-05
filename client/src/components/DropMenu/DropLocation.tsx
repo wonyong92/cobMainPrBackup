30be90d4802127e0825d1ff53f1bb2d00c63c03c
Moondoyeon <mmmdo21@gmail.com>
2022-10-05 10:29:55 +0900

    [Fix]버그픽스

--- client/src/components/DropMenu/DropLocation.tsx
+++ client/src/components/DropMenu/DropLocation.tsx
@@ -1,56 +1,56 @@
 import styled from 'styled-components';
 import { useState, useCallback } from 'react';
-import {location} from '../../constants';
+import { location } from '../../constants';
 
 interface DropProps {
-   name: string;
+  name: string;
 }
 
 const DropLocation = () => {
-    const [isActive, setIsActive] = useState<boolean>(false);
-    const [item, setItem] = useState(null);
-  
-    const onActiveToggle = useCallback(() => {
-      setIsActive((prev) => !prev);
-    }, []);
-  
-    const onSelectItem = useCallback((e:any) => {
-      const targetId = e.target;
-      console.log(e.target.id)
-  
-      if (targetId === 'item_name') {
-        setItem(e.target.parentElement.innertText);
-      } else if (targetId === 'item') {
-        setItem(e.target.innertText);
-      }
-  
-      setIsActive((prev) => !prev);
-    }, []);
-  
-    return (
-      <DropdownContainer>
-        <DropdownBody onClick={onActiveToggle}>
-          {item ? (
-            <>
-              <ItemName>{item}</ItemName>
-            </>
-          ) : (
-            <>
-              <DropdownSelect>지역을 선택해주세요</DropdownSelect>
-            </>
-          )}
-        </DropdownBody>
-        <DropdownMenu isActive={isActive}>
-          {location.map((item) => (
-            <DropdownItemContainer id='item' key={item.lid} onClick={onSelectItem}>
-              <ItemName id='item_name'>{item.name}</ItemName>
-            </DropdownItemContainer>
-          ))}
-        </DropdownMenu>
-      </DropdownContainer>
-    );
-  };
-  
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [item, setItem] = useState(null);
+
+  const onActiveToggle = useCallback(() => {
+    setIsActive((prev) => !prev);
+  }, []);
+
+  const onSelectItem = useCallback((e: any) => {
+    const targetId = e.target;
+    console.log(e.target.id);
+
+    if (targetId === 'item_name') {
+      setItem(e.target.parentElement.innertText);
+    } else if (targetId === 'item') {
+      setItem(e.target.innertText);
+    }
+
+    setIsActive((prev) => !prev);
+  }, []);
+
+  return (
+    <DropdownContainer>
+      <DropdownBody onClick={onActiveToggle}>
+        {item ? (
+          <>
+            <ItemName>{item}</ItemName>
+          </>
+        ) : (
+          <>
+            <DropdownSelect>지역을 선택해주세요</DropdownSelect>
+          </>
+        )}
+      </DropdownBody>
+      <DropdownMenu isActive={isActive}>
+        {location.map((item) => (
+          <DropdownItemContainer id="item" key={item.lid} onClick={onSelectItem}>
+            <ItemName id="item_name">{item.name}</ItemName>
+          </DropdownItemContainer>
+        ))}
+      </DropdownMenu>
+    </DropdownContainer>
+  );
+};
+
 const DropdownContainer = styled.div`
   width: 200px;
   height: 27px;
@@ -70,24 +70,21 @@ const DropdownBody = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top:3px;
-  
+  padding-top: 3px;
 `;
 
 const DropdownSelect = styled.p`
- 
-    font-size: 15px;
-    color: #adaaaa;
-
+  font-size: 15px;
+  color: #adaaaa;
 `;
 
-const DropdownMenu = styled.ul`
+const DropdownMenu = styled.ul<{ isActive: boolean }>`
   display: ${(props) => (props.isActive ? `block` : `none`)};
   width: 400px;
   background-color: white;
   position: absolute;
   border: 2px solid #f4acbb;
-  z-index:999
+  z-index: 999;
 `;
 
 export const DropdownItemContainer = styled.li`
@@ -107,4 +104,4 @@ const ItemName = styled.p`
   font-weight: bold;
 `;
 
-  export default DropLocation;
+export default DropLocation;
