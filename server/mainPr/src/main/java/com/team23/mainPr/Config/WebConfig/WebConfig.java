89c6467f62e83f92b201aa4d0c7fa97bc0c93c14
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-10-11 23:50:49 +0900

    Update WebConfig.java

--- server/mainPr/src/main/java/com/team23/mainPr/Config/WebConfig/WebConfig.java
+++ server/mainPr/src/main/java/com/team23/mainPr/Config/WebConfig/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").exposedHeaders("Authorization").allowedMethods("*").allowCredentials(false).allowedHeaders("*").allowedOrigins(s3Url,frontUrl,"http://localhost:8080");
+        registry.addMapping("/**").exposedHeaders("Authorization").allowedMethods("*").allowCredentials(false).allowedHeaders("*").allowedOrigins(s3Url,frontUrl,"http://localhost:8080","http://localhost:3000");
     }
 
     @Override
